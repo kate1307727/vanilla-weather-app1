853483af1f96b475bc9827baa48c81c20d1232b3
kate1307727 <120455766+kate1307727@users.noreply.github.com>
2023-01-04 20:19:30 +1100

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -21,6 +21,59 @@ function formatDate(timestamp) {
 
   return `${day} ${hours}:${minutes}`;
 }
+function formatDay(timestamp){
+    let date=new Date(timestamp*1000);
+    let day=date.getDay();
+    let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    return days[day];
+}
+function displayForecast(response) {
+    let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+ 
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay,index) {
+   if (index<6){
+
+   
+    forecastHTML += `
+<div class="col-2">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
+                <img
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                <span class="wearher-forecast-temperature-max"> ${Math.round(
+                  forecastDay.temp.max
+                )}° </span>
+                <span class="wearher-forecast-temperature-min"> ${
+                  Math.round(forecastDay.temp.min)
+                }° </span>
+              </div>
+            </div>
+          `;
+            }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  
+}
+
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey = "2b6fdad0cbd018949c50c70f72250726";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -44,6 +97,7 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -91,3 +145,4 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 search("Geelong");
+
